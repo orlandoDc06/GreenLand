8d5f57c1a4fde1c912fd4eab158fd3d89a80ed22
Orlando Del Cid <orlandc410@gmail.com>
2025-06-01 13:21:30 -0600

    arreglo modelo general 4

--- resources/views/maps.blade.php
+++ resources/views/maps.blade.php
@@ -20,12 +20,13 @@
     </div>
 
 </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <style>
 #map-container {
     position: relative;
     width: 1200px;
     height: 1300px;
-    background-image: url('{{ asset("images/MODELO GENERAL.png") }}');
+    background-image: url('{{ asset("images/model.png") }}');
     background-size: cover;
     background-position: center;
     border-radius: 5px;
